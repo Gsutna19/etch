0b55a7db97132a882056c396fc392d0e49a2b630
Felipe Menezes <fe.lipe19menezes@gmail.com>
2022-10-25 12:29:00 -0400

    Create multiple divs inside container

--- js/grid.js
+++ js/grid.js
@@ -1,9 +1,13 @@
 function createGrid() {
-    let gridDiv = document.createElement("div");
-    gridDiv.className = "square";
+    // let gridDiv = document.createElement("div");
+    // gridDiv.className = "square";
 
-    document.getElementById("container").appendChild(gridDiv);
-
+    for ( let i = 0; i < 10; i++) {
+        console.log(i);
+        let gridDiv = document.createElement("div");
+        gridDiv.className = "square";
+        document.getElementById("container").appendChild(gridDiv);
+    }
 
     
 }
