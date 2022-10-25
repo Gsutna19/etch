ee5c8c2a0042d13c26a2dfde9ebd25902759253e
Felipe Menezes <fe.lipe19menezes@gmail.com>
2022-10-25 12:33:11 -0400

    Add button to prompt for size of grid

--- js/grid.js
+++ js/grid.js
@@ -1,9 +1,17 @@
+function setSize() {
+    let size = prompt("Please enter a number for the grid size.");
+
+    if (size != null) {
+        return size;
+    }
+}
+
 function createGrid() {
     // let gridDiv = document.createElement("div");
     // gridDiv.className = "square";
 
     for ( let i = 0; i < 10; i++) {
-        console.log(i);
+        // console.log(i);
         let gridDiv = document.createElement("div");
         gridDiv.className = "square";
         document.getElementById("container").appendChild(gridDiv);
