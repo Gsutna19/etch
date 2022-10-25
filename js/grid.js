9110e6f9b298d10075e4b58832f290eb5cb3f086
Felipe Menezes <fe.lipe19menezes@gmail.com>
2022-10-25 13:15:02 -0400

    Add function to recreate grid

--- js/grid.js
+++ js/grid.js
@@ -1,22 +1,53 @@
+function restartGrid() {
+    let container = document.getElementById("container");
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+}
+
 function setSize() {
     let size = prompt("Please enter a number for the grid size.");
 
-    if (size != null) {
+    if (size > 100) {
+        size = 100;
+        return size;
+    } else if (size != null) {
         return size;
     }
 }
 
-function createGrid(size) {
+function createRows(size) {
     // let gridDiv = document.createElement("div");
     // gridDiv.className = "square";
+
 
     for ( let i = 0; i < size; i++) {
         // console.log(i);
         let gridDiv = document.createElement("div");
-        gridDiv.className = "square";
+        gridDiv.className = "row";
+        gridDiv.id = i;
+        // gridDiv.classList.add = ('square', 'row');
         document.getElementById("container").appendChild(gridDiv);
     }
 }
 
-let gridSize = setSize();
-createGrid(gridSize);
+function populateRows(size) {
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+
+            let gridDiv = document.createElement("div");
+            gridDiv.className = "square";
+            document.getElementById(i).appendChild(gridDiv);
+        }
+    }
+}
+
+function draw() {
+
+    restartGrid();
+
+    let gridSize = setSize();
+
+    createRows(gridSize);
+    populateRows(gridSize);
+}
